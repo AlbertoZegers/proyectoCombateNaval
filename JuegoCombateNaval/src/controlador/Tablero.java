9f3ee52d8706f26d24abff6d73477437aa9b8874
AlbertoZegers <101838814+AlbertoZegers@users.noreply.github.com>
2022-11-04 19:40:33 -0300

    Delete JuegoCombateNaval directory

--- JuegoCombateNaval/src/controlador/Tablero.java
+++
@@ -1,9 +0,0 @@
-package controlador;
-import modelo.Barco;
-import java.util.ArrayList;
-public class Tablero {
-    private ArrayList<Barco> tablero;
-    public Tablero() {
-        this.tablero = new ArrayList<>();
-    }
-}

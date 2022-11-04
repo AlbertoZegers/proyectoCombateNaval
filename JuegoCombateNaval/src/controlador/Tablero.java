91d87a9127dbec9212978571d06072c0645a2b13
AlbertoZegers <101838814+AlbertoZegers@users.noreply.github.com>
2022-11-03 21:38:11 -0300

    Add files via upload

---
+++ JuegoCombateNaval/src/controlador/Tablero.java
@@ -0,0 +1,9 @@
+package controlador;
+import modelo.Barco;
+import java.util.ArrayList;
+public class Tablero {
+    private ArrayList<Barco> tablero;
+    public Tablero() {
+        this.tablero = new ArrayList<>();
+    }
+}

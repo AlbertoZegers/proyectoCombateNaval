5d78d05e3af1b468d51347d1b5e3f0a0103958cc
AlbertoZegers <101838814+AlbertoZegers@users.noreply.github.com>
2022-11-19 22:33:15 -0300

    Add files via upload

---
+++ JuegoCombateNaval/src/modelo/Barco.java
@@ -0,0 +1,35 @@
+package modelo;
+
+public class Barco {
+
+    private String nombre;
+    private char tipo;
+
+    public Barco(String nombre, char tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public Barco() {
+        this.nombre = "agua";
+        this.tipo = 'A';
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+}
+
+
